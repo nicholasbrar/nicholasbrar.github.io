aa9a67ac111bb3d2fdf09efc87f397fa1f4754ea
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2026-02-17 18:17:25 +0000

    Deploying to gh-pages from @ nicholasbrar/nicholasbrar.github.io@1d69d65fb66b4cd2431f35cc80c7fb3dadce0845 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Some of my favorite things I&#39;ve worked on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
