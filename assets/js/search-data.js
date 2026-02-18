6e335c879a472844af093f6c4e059f0ca9105dc1
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2026-02-18 00:50:11 +0000

    Deploying to gh-pages from @ nicholasbrar/nicholasbrar.github.io@fa13d5a515b131ed455ef5944a5b72384d4944d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,9 +424,9 @@ ninja.data = [{
           description: "Where emissions come from",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-household-archetypes",
+          title: 'Household Archetypes',
+          description: "placeholder",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
