a44e2cc039cacc4f91566d46963714c45705cfea
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2026-02-18 00:19:20 +0000

    Deploying to gh-pages from @ nicholasbrar/nicholasbrar.github.io@76bfee4f150364f3bef992c89eae37f9276ea5d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "Environmental and socioeconomic impact dashboard.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-consumption-model",
+          title: 'Consumption Model',
+          description: "High performance log-linear regression model",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
